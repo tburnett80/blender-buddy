f26fadfa93e72ed495913ae8a951824e7170c519
Tim Burnett <tburnett@valoremconsulting.com>
2017-08-15 15:12:52 -0500

    Cleanup

--- mobile/blender-buddy/core.bb/Contracts/ICalculatorManager.cs
+++ mobile/blender-buddy/core.bb/Contracts/ICalculatorManager.cs
@@ -3,6 +3,9 @@ using core.bb.Models;
 
 namespace core.bb.Contracts
 {
+    /// <summary>
+    /// public entry point of the library
+    /// </summary>
     public interface ICalculatorManager
     {
         Task<CalculationResult> CalculateFill(CalculationRequest fillParams);
