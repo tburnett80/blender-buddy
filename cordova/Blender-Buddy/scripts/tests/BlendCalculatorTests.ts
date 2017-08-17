320fabc379e70d748425c5e2e908c05e1faf6a99
Tim Burnett <tburnett@valoremconsulting.com>
2017-08-16 20:18:38 -0500

    cleared up build errors, got sample tests working with jasmine

---
+++ cordova/Blender-Buddy/scripts/tests/BlendCalculatorTests.ts
@@ -0,0 +1,14 @@
+﻿///<reference path="../../node_modules/definitely-typed-jasmine/jasmine.d.ts"/>
+
+//https://www.codeproject.com/Tips/1030700/Testing-Typescript-with-Jasmine-and-Chutzpah
+describe("Sample test", () => {
+    let varaiable = true;
+
+    it("should match", () => {
+        expect(varaiable).toBe(true);
+    });
+
+    it("should not match", () => {
+        expect(varaiable).toBe(false);
+    });
+});
