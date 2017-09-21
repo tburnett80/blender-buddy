fc8bfb73ace3e08cb1a2063a12b0960c551a1591
tburnett80@gmail.com <tburnett80@gmail.com>
2017-09-21 09:11:03 -0500

    started wiring up END / EAD component

---
+++ blender-buddy-web/ClientApp/app/components/endead/endead.component.ts
@@ -0,0 +1,10 @@
+﻿import { Component } from '@angular/core';
+
+@Component({
+    selector: 'endead',
+    templateUrl: './endead.component.html',
+    styleUrls: ['./endead.component.css']
+})
+export class EndEadComponent {
+
+}
